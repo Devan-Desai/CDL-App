067a5bd72fc90570d795e8be239adcbe879c9df7
Devan Desai <devandesai@trentu.ca>
2026-01-06 17:07:21 -0500

    Initial Commit

---
+++ CDL App/CDL_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CDL_AppApp.swift
+//  CDL App
+//
+//  Created by Devan Desai on 2026-01-06.
+//
+
+import SwiftUI
+
+@main
+struct CDL_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
